8a96ec1342c45ebfdf5a2cb2a45beea3ba6b177f
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2024-01-10 13:53:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 //your JS code here. If required.
-const student={
-	nmae:"Abu Salman"
-}
+// Create a student object with a name property
+const student = {
+  name: "John Doe"
+};
 
-Object.prototype.getkeys(){
+// Add getKeys method to the Object prototype
+Object.prototype.getKeys = function() {
+  // Get all the keys of the current object
+  return Object.keys(this);
+};
 
-return Object.keys(this);
-}
+// Test the getKeys method on the student object
+const keysArray = student.getKeys();
+
+// Display the result
+console.log(keysArray);
 
